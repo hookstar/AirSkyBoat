8a5634cd7a6f2f0d157e39d27a0e00a9ef4894d4
hookstar <lance.hooker@gmail.com>
2023-03-06 11:27:10 -0500

    Update Nimble Snap
    Update  Nimble Snap
    
    https://ffxiclopedia.fandom.com/wiki/Nimble_Snap

--- scripts/globals/mobskills/nimble_snap.lua
+++ scripts/globals/mobskills/nimble_snap.lua
@@ -13,11 +13,15 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
-    local numhits = 3
-    local accmod = 1
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, 1, xi.mobskills.physicalTpBonus.DMG_VARIES, 3, 3.25, 3.5)
-    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
-    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
+    if mob:isMobType(xi.mobskills.mobType.NOTORIOUS) then
+        numhits = 3
+    else
+        numhits = 1
+    end
+        accmod = 1
+        info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, 1, xi.mobskills.physicalTpBonus.DMG_VARIES, 1, 1.25, 1.5) --todo verify NM's FTP on retail/jimmy
+        dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
+        target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
     return dmg
 end
 
