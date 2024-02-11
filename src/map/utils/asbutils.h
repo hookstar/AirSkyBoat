26620c5b2fad40a2e1750dc8439bccc6b1c16c82
Michael <121089569+Shozokui@users.noreply.github.com>
2024-02-11 15:18:40 +0800

    [core] Add `asbutils` namespace for re-defining upstream functions.
    This is intended to be used as a stopgap for code which cannot be reconciled.

---
+++ src/map/utils/asbutils.h
@@ -0,0 +1,37 @@
+/*
+===========================================================================
+
+  Copyright (c) 2024 AirSkyBoat Dev Teams
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+
+===========================================================================
+*/
+
+#ifndef _ASBUTILS_H
+#define _ASBUTILS_H
+
+#include "common/cbasetypes.h"
+#include "common/mmo.h"
+
+namespace asbutils
+{
+    // Namespace for overriden utility functions and rerouted functions
+
+    // Format:
+    // <Source> Example: utils/battleutils
+    // <What this modifies from LSB and why is it not reconcilable>
+};
+
+#endif 
